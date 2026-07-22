73aa4bcb4303fd91d544162b994e188399fdbdca
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:32:33 +0800

    Automated bulk repository update

---
+++ src/CanServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Can;
+
+use Rimba\Base\BitesServiceProvider;
+
+
+class CanServiceProvider extends BitesServiceProvider
+{
+
+    protected function bootPackage(): void
+    {
+        //
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
