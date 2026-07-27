a2eaa42b42ea0d2e75a3046e9b5d688c14821fe9
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-27 10:12:32 +0800

    Update 2026-07-27 10:12

--- src/CanServiceProvider.php
+++ src/CanServiceProvider.php
@@ -6,15 +6,17 @@ namespace Rimba\Can;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class CanServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
