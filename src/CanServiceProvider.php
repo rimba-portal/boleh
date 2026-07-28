c8e5fb3e87965f6a1b0d58e28092b83390c51c94
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-28 08:52:42 +0800

    Release v1.2.2

--- src/CanServiceProvider.php
+++ src/CanServiceProvider.php
@@ -12,6 +12,7 @@ class CanServiceProvider extends BitesServiceProvider
 
     protected function bootPackage(): void
     {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
     protected function registerPackage(): void
