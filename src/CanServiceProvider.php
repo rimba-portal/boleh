fb19f013a0d953b443275f8c53e233f4871d3264
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 16:14:54 +0800

    Release v1.2.8

--- src/CanServiceProvider.php
+++ src/CanServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Can;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class CanServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
